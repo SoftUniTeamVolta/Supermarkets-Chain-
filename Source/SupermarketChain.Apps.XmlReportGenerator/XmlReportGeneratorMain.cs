ac96e400ca2ba0d1bbdb2374d819ed4235083ba1
Daniel <petrovaliev95@abv.bg>
2015-03-18 00:38:33 +0200

    problem 4 XML sales report solved, other bug fixes

--- Source/SupermarketChain.Apps.XmlReportGenerator/XmlReportGeneratorMain.cs
+++ Source/SupermarketChain.Apps.XmlReportGenerator/XmlReportGeneratorMain.cs
@@ -1,4 +1,10 @@
-﻿namespace SupermarketChain.Apps.XmlReportGenerator
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+using SupermarketChain.Data.DataContext;
+
+namespace SupermarketChain.Apps.XmlReportGenerator
 {
     using SupermarketChain.Common.Utils;
     using SupermarketChain.Data.Models.SQLServerModels;
@@ -13,9 +19,9 @@
         static XmlReportGeneratorMain()
         {
             string outputDir = ConfigUtils.GetAppSetting("OutputDir");
-            if(string.IsNullOrEmpty(outputDir))
+            if (string.IsNullOrEmpty(outputDir))
             {
-                outputDir = "./"; 
+                outputDir = "./";
             }
 
             XmlReportGeneratorMain.DocumentOutputPath = outputDir + XmlReportGeneratorMain.DocumentName;
@@ -23,19 +29,51 @@
 
         public static void Main(string[] args)
         {
-            DateTime startDate;
-            DateTime endDate;
+            DateTime startDate = new DateTime(1300, 01, 01);
+            DateTime endDate = new DateTime(2040, 01, 01);
 
-            ValidateInputArguments(args, out startDate, out endDate);
-
-            ICollection<Product> data = new List<Product>();
-
-            GenerateXmlReport(data, startDate, endDate);
+            GenerateXmlReport(startDate, endDate);
+            Console.WriteLine("Xml report has been generated!");
         }
 
-        private static void GenerateXmlReport(ICollection<Product> data, DateTime startDate, DateTime endDate)
+        private static void GenerateXmlReport(DateTime startDate, DateTime endDate)
         {
-            throw new NotImplementedException();
+            using (var context = new MsDataContext())
+            {
+                var doc = new XmlDocument();
+
+                XmlDeclaration declaration = doc.CreateXmlDeclaration("1.0", null, null);
+                doc.AppendChild(declaration);
+
+                XmlElement rootNode = doc.CreateElement("sales");
+
+                var vendors = context
+                    .Vendors
+                    .Where(v =>
+                        v.Sales
+                            .Any(s => s.Date >= startDate && s.Date <= endDate))
+                    .Include(v => v.Sales);
+
+                foreach (var vendor in vendors)
+                {
+                    XmlElement currVendorNode = doc.CreateElement("sale");
+                    currVendorNode.SetAttribute("vendor", vendor.Name);
+
+                    foreach (var sale in vendor.Sales)
+                    {
+                        XmlElement currSaleNode = doc.CreateElement("summary");
+                        currSaleNode.SetAttribute("date", sale.Date.ToString("dd-MMM-yyyy"));
+                        currSaleNode.SetAttribute("total-sum", sale.Sum.ToString());
+
+                        currVendorNode.AppendChild(currSaleNode);
+                    }
+
+                    rootNode.AppendChild(currVendorNode);
+                }
+
+                doc.AppendChild(rootNode);
+                doc.Save("../../../generated-files/xml-reports.xml");
+            }
         }
 
         private static void ValidateInputArguments(string[] args, out DateTime startDate, out DateTime endDate)
