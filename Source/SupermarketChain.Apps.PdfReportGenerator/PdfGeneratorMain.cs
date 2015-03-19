5718bc20679fc391c2327c90679750a8ffa1e61a
Vassil Nikov <vassiln@yahoo.com>
2015-03-20 01:00:06 +0200

    pdf generator alfa release done :)

--- Source/SupermarketChain.Apps.PdfReportGenerator/PdfGeneratorMain.cs
+++ Source/SupermarketChain.Apps.PdfReportGenerator/PdfGeneratorMain.cs
@@ -6,7 +6,12 @@
     using SupermarketChain.Data.Models.SQLServerModels;
     using System;
     using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Data.Entity.Core.Objects;
     using System.IO;
+    using System.Linq;
+    using Data.DataContext;
+    using Data.DataContext.Repositories;
 
     public static class PdfGeneratorMain
     {
@@ -18,7 +23,7 @@
             string outputDir = ConfigUtils.GetAppSetting("OutputDir");
             if(string.IsNullOrEmpty(outputDir))
             {
-                outputDir = "./"; 
+                outputDir = @"D:\Users\"; 
             }
 
             PdfGeneratorMain.DocumentOutputPath = outputDir + PdfGeneratorMain.PdfDocumentName;
@@ -26,14 +31,26 @@
 
         public static void Main(string[] args)
         {
-            DateTime startDate;
-            DateTime endDate;
+            // Initialize only for testing! Remove when done.
+            DateTime startDate = new DateTime(2015, 03, 16);
+            DateTime endDate = new DateTime(2015, 03, 20);
 
-            PdfGeneratorMain.ValidateInputArguments(args, out startDate, out endDate);
+            //PdfGeneratorMain.ValidateInputArguments(args, out startDate, out endDate);
 
-            ICollection<Product> data = new List<Product>();
-
-            Document pdfReport = PdfGeneratorMain.GeneratePdfReport(data, startDate, endDate);
+            try
+            {
+                using (var context = new MsDataContext())
+                {
+                    SortedDictionary<DateTime, ICollection<Sale>> data = PdfGeneratorMain.FetchDataFromDatabase(context, startDate, endDate);
+                    Document pdfReport = PdfGeneratorMain.GeneratePdfReport(data, startDate, endDate);
+                }
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+           
         }
 
         private static void ValidateInputArguments(string[] args, out DateTime startDate, out DateTime endDate)
@@ -53,15 +70,74 @@
             }
         }
 
-        private static Document GeneratePdfReport(ICollection<Product> data, DateTime startDate, DateTime endDate)
+        private static SortedDictionary<DateTime, ICollection<Sale>> FetchDataFromDatabase(IDataContext context, DateTime startDate, DateTime endDate)
         {
-            Document pdf = new Document();
-            PdfWriter writer = PdfWriter.GetInstance(pdf, new FileStream(PdfGeneratorMain.DocumentOutputPath, FileMode.Create));
-            pdf.Open();
+            var sales = new GenericRepository<Sale>(context);
+            var result = sales.Find(s => DbFunctions.TruncateTime(s.Date) >= startDate && DbFunctions.TruncateTime(s.Date) <= endDate).ToList();
 
-            // do some work
+            var sortedData = new SortedDictionary<DateTime, ICollection<Sale>>();
+            foreach (var sale in result)
+            {
+                var date = sale.Date;
+                if (!sortedData.ContainsKey(sale.Date))
+                {
+                    sortedData.Add(sale.Date, new List<Sale>());
+                }
+                else
+                {
+                    sortedData[date].Add(sale);
+                }
+            }
 
-            pdf.Close();
+            return sortedData;
+        }
+
+        private static Document GeneratePdfReport(SortedDictionary<DateTime, ICollection<Sale>> saleData, DateTime startDate, DateTime endDate)
+        {
+            
+            Document pdf = new Document(PageSize.A4, 2, 2, 2, 2);
+            PdfPTable table = new PdfPTable(5); // 5-number of columns
+            table.HorizontalAlignment = 1; // 0-left, 1-center, 2-right
+            table.SpacingBefore = 20f;
+            table.SpacingAfter = 20f;
+            //table.HeaderRows = 1;
+            using (PdfWriter writer = PdfWriter.GetInstance(pdf, new FileStream("pdfReport.pdf", FileMode.Create)))
+            {
+                PdfPCell cell = new PdfPCell(new Phrase("Aggregated Sales Report"));
+                cell.Colspan = 5;
+                cell.HorizontalAlignment = 1;
+                table.DefaultCell.HorizontalAlignment = 1;
+                table.AddCell(cell);
+                table.AddCell("Product");
+                table.AddCell("Quantity");
+                table.AddCell("Unit Price");
+                table.AddCell("Location");
+                table.AddCell("Sum");
+                foreach (var date in saleData)
+                {
+                    var dateRow = new PdfPCell(new Phrase(date.ToString()));
+                    dateRow.Colspan = 5;
+                    //secondRow.BackgroundColor.Darker();
+                    table.AddCell(dateRow);
+                    //table.getDefaultCell().setBackgroundColor(BaseColor.LIGHT_GRAY);
+                    foreach (var sale in date.Value)
+                    {
+                        table.AddCell(sale.Product.ToString());
+                        table.AddCell(sale.Quantity.ToString());
+                        table.AddCell(sale.UnitPrice.ToString());
+                        table.AddCell(sale.Supermarket.ToString());
+                        table.AddCell(sale.Sum.ToString());
+                    }
+                }
+
+                pdf.Open();
+                pdf.Add(table);
+
+                // do some work
+
+                pdf.Close();
+            }
+            
             return pdf;
         }
     }
