66873596266785062bf4978a0607c7112dd6e3b1
Daniel <petrovaliev95@abv.bg>
2015-03-20 11:17:43 +0200

    commit bug fixed

---
+++ Source/SupermarketChain.Apps.XmlExpenseImport/XmlImport.cs
@@ -0,0 +1,82 @@
+﻿namespace SupermarketChain.Apps.XmlExpenseImport
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Xml.Linq;
+    
+    using Data.DataContext;
+    using Data.Models.SQLServerModels;
+
+    public static class XmlImport
+    {
+        private const string DocumentPath = "../../../../files-for-import/Sample-Vendor-Expenses.xml";
+
+        public static void ImportExpenses()
+        {
+            XDocument doc = new XDocument();
+
+            try
+            {
+                doc = XDocument.Load(DocumentPath);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("File not found! See trace: " + e.StackTrace);
+            }
+
+            var expensesByVendor =
+                (from vendor in doc.Descendants("vendor")
+                 select new
+                 {
+                     Name = vendor.Attribute("name").Value,
+                     Expenses = vendor
+                         .Elements("expenses")
+                         .Select(e =>
+                             new
+                             {
+                                 Month = DateTime.Parse(e.Attribute("month").Value),
+                                 Sum = decimal.Parse(e.Value)
+                             })
+                         .ToList()
+                 })
+                    .ToList();
+
+            foreach (var vendor in expensesByVendor)
+            {
+                foreach (var expense in vendor.Expenses)
+                {
+                    AddExpense(vendor.Name, expense.Month, expense.Sum);
+                }
+            }
+
+            Console.WriteLine("Expenses has been added to DB.");
+            Console.WriteLine();
+        }
+
+        private static void AddExpense(string vendorName, DateTime expenseDate, decimal expenseSum)
+        {
+            using (var context = new MsDataContext())
+            {
+                int vendorId = 0;
+
+                if (context.Vendors.Any(v => v.Name == vendorName))
+                {
+                    vendorId = context.Vendors.FirstOrDefault(v => v.Name == vendorName).Id;
+                }
+                else
+                {
+                    var newVendor = new Vendor {Name = vendorName};
+
+                    context.Vendors.Add(newVendor);
+                    context.SaveChanges();
+
+                    vendorId = newVendor.Id;
+                }
+
+                context.Expenses.Add(new Expense {VendorId = vendorId, Date = expenseDate, Sum = expenseSum});
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Source/SupermarketChain.Apps.XmlReportGenerator/XmlReportGenerator.cs
@@ -0,0 +1,58 @@
+﻿namespace SupermarketChain.Apps.XmlReportGenerator
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Xml;
+    using Common.Utils;
+    using Data.DataContext;
+
+    public static class XmlReportGenerator
+    {
+        private const string DocumentName = "SalesReport.xml";
+        private const string OutputDir = "../../../generated-files";
+
+        public static void GenerateXmlReport(DateTime startDate, DateTime endDate)
+        {
+            using (var context = new MsDataContext())
+            {
+                var doc = new XmlDocument();
+
+                XmlDeclaration declaration = doc.CreateXmlDeclaration("1.0", null, null);
+                doc.AppendChild(declaration);
+
+                XmlElement rootNode = doc.CreateElement("sales");
+
+                var vendors = context
+                    .Vendors
+                    .Where(v =>
+                        v.Sales
+                            .Any(s => s.Date >= startDate && s.Date <= endDate))
+                    .Include(v => v.Sales);
+
+                foreach (var vendor in vendors)
+                {
+                    XmlElement currVendorNode = doc.CreateElement("sale");
+                    currVendorNode.SetAttribute("vendor", vendor.Name);
+
+                    foreach (var sale in vendor.Sales)
+                    {
+                        XmlElement currSaleNode = doc.CreateElement("summary");
+                        currSaleNode.SetAttribute("date", sale.Date.ToString("dd-MMM-yyyy"));
+                        currSaleNode.SetAttribute("total-sum", sale.Sum.ToString());
+
+                        currVendorNode.AppendChild(currSaleNode);
+                    }
+
+                    rootNode.AppendChild(currVendorNode);
+                }
+
+                doc.AppendChild(rootNode);
+                doc.Save(OutputDir + "/" + DocumentName);
+            }
+
+            Console.WriteLine("Xml report has been generated!");
+            Console.WriteLine();
+        }
+    }
+}
